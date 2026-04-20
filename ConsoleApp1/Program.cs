2ad2e3cc9f1bdaf939c24029f7192192e506b564
lengochuynhmai87-prog <lengochuynhmai87@gmail.com>
2026-04-20 11:13:03 +0700

    CODE

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using ConsoleApp1;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static TCPClient client = new TCPClient();
+        static TCPServer sever = new TCPServer();
+        static void Main(string[] args)
+        {
+            //Ket noi
+            sever.StartSv();
+            client.Start("127.0.0.1", 9999);
+            while (true)
+            {
+                Console.WriteLine("Nhap path");
+                string path = Console.ReadLine(); //lấy trên ofd
+                client.SendFile(path);
+                sever.rcv();
+            }
+
+            
+        }
+        
+    }
+}
