acdae1a1453150877dbfc87c84606f3f451ae90d
lengochuynhmai87-prog <lengochuynhmai87@gmail.com>
2026-04-21 11:46:37 +0700

    Update

---
+++ Downloads/Translet_file-master/Translet_file-master/WinFormsApp1/Program.cs
@@ -0,0 +1,21 @@
+
+namespace WinFormsApp1
+{
+    public static class Program
+    {
+        public static Form1 form = new Form1();
+
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            Application.Run(form);
+        }
+    }
+}
---
+++ Downloads/Translet_file-master/Translet_file-master/WinFormsApp1/TCPServer.cs
@@ -0,0 +1,175 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WinFormsApp1
+{
+    public class TCPServer
+    {
+        Socket? sckSever;
+        public static int port = 9999;
+        public Form1 f;
+        public string Folder = "Receive_File";
+        public static int BUFFERSIZE = 65536;
+
+
+
+        public class ClientState
+        {
+            public Socket? sckClient;
+            public FileStream? fs;
+            public byte[] buffer = new byte[BUFFERSIZE];
+            public bool isHeader = true;
+            public long countReceive = 0;
+            public long totalReceive = 0;
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork) // IPv4
+                {
+                    return ip.ToString();
+                }
+            }
+
+            return "Không tìm thấy IP";
+        }
+        public void StartSv()
+        {
+            f = Program.form;
+
+            if (!Directory.Exists(Folder))
+            {
+                Directory.CreateDirectory(Folder);
+            }
+            while (port > 0)
+            {
+                try
+                {
+                    f.textBox1.Text = GetLocalIPAddress();
+                    f.textBox2.Text = port.ToString();
+
+                    sckSever = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    IPEndPoint ep = new IPEndPoint(IPAddress.Any, port);
+                    sckSever.Bind(ep);
+
+                    sckSever.Listen(4);
+
+                    sckSever.BeginAccept(new AsyncCallback(OnAccept), sckSever);
+
+                    break;
+                }
+                catch
+                {
+                    port--;
+                }
+            }
+
+        }
+
+        private void OnAccept(IAsyncResult ar)
+        {
+            ClientState st = new ClientState();
+            Socket sck = (Socket)ar.AsyncState;
+            st.sckClient = sck.EndAccept(ar);
+            f.Invoke(() =>
+            {
+                f.label2.Text = "Đã Được Kết Nối!";
+            });
+            sckSever.BeginAccept(new AsyncCallback(OnAccept), sckSever);
+           
+            st.sckClient.BeginReceive(st.buffer, 0, st.buffer.Length, SocketFlags.None, OnReceive, st);
+
+
+
+        }
+
+        private void OnReceive(IAsyncResult ar)
+        {
+            ClientState st = (ClientState)ar.AsyncState;
+            int n = st.sckClient.EndReceive(ar);
+            if (n <= 0) return;
+
+            if (st.isHeader)
+            {
+                string msg = Encoding.UTF8.GetString(st.buffer, 0, n);
+
+                if (!msg.Contains("|"))
+                {
+                    st.sckClient.BeginReceive(st.buffer, 0, BUFFERSIZE, SocketFlags.None, OnReceive, st);
+                    return;
+                }
+
+                string[] header = msg.Split('|');
+                string[] parts = header[0].Split(';');
+
+                if (parts.Length < 2)
+                {
+                    st.sckClient.BeginReceive(st.buffer, 0, BUFFERSIZE, SocketFlags.None, OnReceive, st);
+                    return;
+                }
+
+                string fileName = parts[0];
+                f.Invoke(() =>
+                {
+                    f.textBox4.Text = fileName;
+                });
+
+                st.totalReceive = long.Parse(parts[1]);
+
+                st.fs = new FileStream(Path.Combine(Folder, fileName), FileMode.Create);
+
+                int headerByteCount = Encoding.UTF8.GetByteCount(header[0] + "|");
+
+                int fileInHeader = n - headerByteCount;
+
+                if (fileInHeader > 0)
+                {
+                    st.fs.Write(st.buffer, headerByteCount, fileInHeader);
+                    st.countReceive += fileInHeader;
+                }
+
+                st.isHeader = false;
+                st.sckClient.BeginReceive(st.buffer, 0, BUFFERSIZE, SocketFlags.None, OnReceive, st);
+            }
+            else
+            {
+                st.fs.Write(st.buffer, 0, n);
+                st.countReceive += n;
+
+                int percent = (int)(st.countReceive * 100 / st.totalReceive);
+                f.Invoke(() =>
+                {
+                    f.label7.Text = percent.ToString() + "%";
+                    f.progressBar2.Value = percent;
+                });
+            }
+            if (st.countReceive < st.totalReceive)
+            {
+                
+                st.sckClient.BeginReceive(st.buffer, 0, BUFFERSIZE, SocketFlags.None, OnReceive, st);
+            }
+            else 
+            {
+                st.fs.Close();
+                st.fs = null;
+
+                st.isHeader = true;
+                st.countReceive = 0;
+                st.totalReceive = 0;
+                f.Invoke(() =>
+                {
+                    f.richTextBox2.Text += "Đã nhận " + f.textBox4.Text + "\n";
+                });
+
+                Console.WriteLine("Đã nhận file thành công!");
+            }
+
+            
+        }
+    }
+}
